ea5250b190312b6539b7efb79c29e1ff5f6d9872
Joe Corwin <joe.corwin@gmail.com>
2017-11-03 17:05:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,64 @@
-require 'open-uri'
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
+#
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read(index_url)
+    flatiron_students = Nokogiri::HTML(html)
+
+    students = []
+
+    flatiron_students.css("div.student-card").each do |s|
+      student_hash = {
+        :name => s.css("h4.student-name").text,
+        :location => s.css("p.student-location").text,
+        :profile_url => s.css("a").attribute("href").value
+        }
+
+      students << student_hash
+    end
+
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    html = File.read(profile_url)
+    sp = Nokogiri::HTML(html)
 
+    profile = {}
+
+    profile_links = sp.css("div.social-icon-container a").map { |link| link.attribute('href').value}
+# Scraping social links
+    profile_links.each do |link|
+      if link.include?('twitter')
+        profile[:twitter] = link
+      elsif link.include?('linkedin')
+        profile[:linkedin] = link
+      elsif link.include?('github')
+        profile[:github] = link
+      else link.include?('http:')
+        profile[:blog] = link
+      end
+    end
+
+# Scraping bio and quote
+    profile[:bio] = sp.css("div.bio-content.content-holder p").text
+    profile[:profile_quote] = sp.css("div.profile-quote").text
+
+    profile
+  end
 end
 
+
+
+#   # # ///SELECTOR TESTER////
+#
+#   # def scrape_index_page
+#   #   html = File.read("./fixtures/student-site/index.html")
+#   #   s = Nokogiri::HTML(html)
+#   #   binding.pry
+#   # end
+#   # scrape_index_page
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,25 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students)
+    students.each do |s|
+      Student.new(s)
+    end
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(profile)
+    profile.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
-    
+    @@all
   end
 end
-
